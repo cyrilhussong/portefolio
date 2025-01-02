0d27dba838bb1d1b66c3fc013a5aab9b01b5a8d1
cyrilhussong <cyrilhussong347@gmail.com>
2025-01-02 13:41:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+  // Gestion du formulaire de contact
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Message envoyé !");
+    // Ici, ajoutez le code pour envoyer le formulaire via une API ou un service d'email.
+  });
+
+
